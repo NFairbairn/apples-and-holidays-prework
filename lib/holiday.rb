c957126dd26cf891064612cc12fb3a37f511ab05
Noah Fairbairn <noahfairbairn@gmail.com>
2019-03-02 18:01:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |keys, values|
     values.each do |holidays, supplies|
-      binding.pry
+
     end
   end
 end
