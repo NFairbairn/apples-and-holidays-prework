8144e9f716eba33dda4f2e8a5919e86ecc9360b1
Noah Fairbairn <noahfairbairn@gmail.com>
2019-03-02 17:27:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,6 +79,6 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  bbq_values = holiday_hash.map{|x| x.values[0]}
+  bbq_values = holiday_hash.map{|x| x.values_at[0]}
   bbq_values
 end
